a70005182cb1ff3ca20a45e77a5d28348ecdb973
ArrudaaJunior <juniorarruda15@gmail.com>
2024-07-11 15:10:45 -0300

    Melhorando a função escolha_opção com o Try e except

--- app.py
+++ app.py
@@ -22,28 +22,38 @@ def finalizar__app():
     os.system('cls')
     print('App finalizado\n')
 
+def opcao_invalida():
+    print('Opção inválida!\n')
+    input('Digite uma tecla para voltar ao menu principal: ')
+    main()
+
 def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        print(f'Você escolheu a opção {opcao_escolhida}')
 
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    print(f'Você escolheu a opção {opcao_escolhida}')
+        match opcao_escolhida:
+            case 1:
+                print('Cadastrar restaurante')
+            case 2:
+                print('Listar restaurantes')
+            case 3:
+                print('Ativar restaurante')
+            case 4:
+                finalizar__app()
+            case _:
+                opcao_invalida()
+    except:
+        opcao_invalida()
 
-    match opcao_escolhida:
-        case 1:
-            print('Cadastrar restaurante')
-        case 2:
-            print('Listar restaurantes')
-        case 3:
-            print('Ativar restaurante')
-        case 4:
-            finalizar__app()
 
 def main():
-
+    os.system('cls')
     exibir_nome_do_app()
     exibir_opcoes()
     escolher_opcao()
 
 if __name__ == '__main__':
-    
+
     main()
     
